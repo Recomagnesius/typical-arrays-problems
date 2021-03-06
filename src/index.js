b70c6a37254a6959856f8395a92810ad4b5d14ef
Recomagnesius <buzhor.danyl@gmail.com>
2021-03-06 15:32:01 +0200

    fail

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array != undefined) {
+    let minArray = array;
+    minArray.sort((a, b) => b - a);
+    return minArray[0];
+  }
+  else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array != undefined) {
+    let maxArray = array;
+    maxArray.sort((a, b) => b - a);
+    maxArray = maxArray[maxArray.length - 1];
+    return maxArray;
+  }
+  else {
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array != undefined) {
+    var sum = 0;
+    for(var i = 0; i < array.length; i++){
+      sum += array[i];
+      return sum / array.length;
+    }
+  }
+  else {
+    return 0;
 }
